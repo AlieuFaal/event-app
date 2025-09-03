07e6504e7f06400bf5895ff9a6007b6455ab01c3
Alieu Faal <alieufaal10@gmail.com>
2025-09-03 23:11:40 +0200

    feat: Add LiveTonightImage export to assets index

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,2 @@
 export { default as DiscJockeyImage } from "src/assets/images/nereid-ndreu-TkW0iUTa2Qg-unsplash.jpg";
+export { default as LiveTonightImage } from "src/assets/images/ahma-9oZc1-WlzAM-unsplash.jpg"
